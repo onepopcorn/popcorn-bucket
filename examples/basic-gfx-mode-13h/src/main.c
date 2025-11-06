02f36cc904d8c9759efad40fcf74ebf3068d6358
onepopcorn <onepopcorn.dev@gmail.com>
2025-11-06 22:26:18 +0100

    fix: improved video funcions and variable types

--- examples/basic-gfx-mode-13h/src/main.c
+++ examples/basic-gfx-mode-13h/src/main.c
@@ -1,12 +1,10 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <conio.h>
 #include "video.h"
 #include <sys/nearptr.h>
 #include <string.h>
 
-#define MODE_13h 0x13
-#define MODE_Text 0x03
-#define VIDEO_MEM_START 0xA0000
 #define SCREEN_WIDTH 320
 
 int main(int argc, char* argv[]) {
@@ -17,14 +15,14 @@ int main(int argc, char* argv[]) {
     }
 
     // Set VGA mode to 13h
-    setVideoMode(MODE_13h);
+    setVideoMode(VIDEO_MODE_13h);
 
     // wait for vsync before writting to VRAM
     waitVSync();
     uint8_t* screen_buffer = (uint8_t*)(VIDEO_MEM_START + __djgpp_conventional_base);
 
-    uint8_t x = 40, y = 40;
-    uint8_t w = 50, h = 50;
+    int x = 40, y = 40;
+    int w = 50, h = 50;
 
     // Draw a red square of 50x50 px writting
     // directly to video memory
@@ -33,7 +31,7 @@ int main(int argc, char* argv[]) {
     //     Writing to video memory directly is not what you would
     //     do in a real game, it's used here for the sake of simplicity
     //     Refere to more advance examples to see better rendering techniques
-    for (uint8_t row = 0; row < h; row++) {
+    for (int row = 0; row < h; row++) {
         memset(screen_buffer + x + (y + row) * SCREEN_WIDTH, 0x04, w);
     }
 
@@ -41,7 +39,7 @@ int main(int argc, char* argv[]) {
     }
 
     // Go back to text mode
-    setVideoMode(MODE_Text);
+    setVideoMode(VIDEO_MODE_Text);
 
     // Enable DJGPP memory protection again
     __djgpp_nearptr_disable();
